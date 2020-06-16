def65c5507deaafbc1d8f8e35ad5337379800092
sinta3 <sinta.tri38@gmail.com>
2020-06-16 15:41:15 +0700

    add all file

---
+++ script.js
@@ -0,0 +1,7 @@
+let newContent = document.getElementById("content");
+let newText = document.getElementById("text");
+document.body.style.backgroundColor = "black";
+
+newText.style.color = "blue";
+newText.style.fontSize = "50px";
+newText.style.fontFamily = "Arial , Georgia , Serif";
